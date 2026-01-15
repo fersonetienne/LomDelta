82a628562ebc7262920b04be40a530412d1c9aa0
fersonetienne <etienneferson@gmail.com>
2026-01-15 17:30:52 -0500

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "LomDelta",
+  description: "LomDelta Marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
