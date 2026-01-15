b43668bb45ad14ed0322358e31b9fabf1238e704
fersonetienne <etienneferson@gmail.com>
2026-01-15 17:32:55 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>LomDelta Marketplace</h1>
+      <p>Bienvenue sur LomDelta</p>
+    </main>
+  );
+}
